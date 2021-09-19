52cc296db40ed0e266cc8b6ecedd74eaf84bc838
Kehinde Adedokun <ken_adex@yahoo.com>
2021-09-19 06:22:12 +0100

    If-else using the ternary operator

---
+++ section_2/ternaryOp.js
@@ -0,0 +1,8 @@
+let bill = 430;
+let tip = bill >= 50 && bill <= 300 ? (15 / 100) * bill : (20 / 100) * bill;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value = ${
+    bill + tip
+  }`
+);
