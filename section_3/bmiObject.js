5de20e722853567576971b686bb25457f6655080
Kehinde Adedokun <ken_adex@yahoo.com>
2021-10-03 04:35:05 +0100

    BMI using object and the this keyword

---
+++ section_3/bmiObject.js
@@ -0,0 +1,31 @@
+const person1 = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const person2 = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+if (person1.calcBMI() > person2.calcBMI()) {
+  console.log(
+    `${person1.fullName}'s BMI (${person1.bmi}) is higher than ${person2.fullName}'s (${person2.bmi})!`
+  );
+} else {
+  console.log(
+    `${person2.fullName}'s BMI (${person2.bmi}) is higher than ${person1.fullName}'s (${person1.bmi})!`
+  );
+}
+
+// console.log(markBMI, johnBMI, markHigherBMI);
